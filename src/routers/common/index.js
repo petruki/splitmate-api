0df51b0db5abd6d3f24a053278d93da43ad00511
petruki <31597636+petruki@users.noreply.github.com>
2020-11-27 17:11:39 -0800

    Added document type on NotFoundException

--- src/routers/common/index.js
+++ src/routers/common/index.js
@@ -2,7 +2,7 @@ function responseException(res, err, code) {
     if (err instanceof PermissionError) {
         res.status(401).send({ error: err.message, code: 401 });
     } else if (err instanceof NotFoundError) {
-        res.status(404).send({ error: err.message });
+        res.status(404).send({ error: err.message, document: err.document });
     } else if (err instanceof BadRequest) {
         res.status(400).send({ error: err.message });
     } else {
@@ -22,6 +22,7 @@ class NotFoundError extends Error {
     constructor(document) {
         super(`'${document}' not found`);
         this.name = this.constructor.name;
+        this.document = document;
         Error.captureStackTrace(this, this.constructor);
     }
 }
